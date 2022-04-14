bf7a78802bd56d95e63de43306cc741b1fdefed4
JoeJoePotato <34780073+JoeJoePotato@users.noreply.github.com>
2022-04-13 18:30:40 -0700

    hoping we didn't make things worse

--- Giraffe-Search-2/src/main/java/resources/ApiDocConfig.java
+++ Giraffe-Search-2/src/main/java/resources/ApiDocConfig.java
@@ -1,4 +1,4 @@
-package org.jointheleague.level7.cheetah.config;
+package resources;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
